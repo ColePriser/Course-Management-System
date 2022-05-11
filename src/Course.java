dd969846b767c75846086eb647577719990389ba
ColePriser <cpriser@purdue.edu>
2022-05-11 18:14:09 -0500

    created fields, constructor, getters and setters

---
+++ src/Course.java
@@ -0,0 +1,25 @@
+public class Course {
+    private String courseName;
+    private int courseID;
+
+    public Course(String courseName, int courseID) {
+        this.courseName = courseName;
+        this.courseID = courseID;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public int getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(int courseID) {
+        this.courseID = courseID;
+    }
+}
