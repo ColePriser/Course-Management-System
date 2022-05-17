06a8335308f47e49eecebd9282a9744da38d3f17
ColePriser <cpriser@purdue.edu>
2022-05-17 17:00:55 -0500

    Main.java now creates a connection between Server.java and Client.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
+import java.lang.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(2361);
+        while (true) {
+            Socket socket = serverSocket.accept();
+            Server server = new Server(socket);
+            new Thread(server).start();
+        }
     }
 }
