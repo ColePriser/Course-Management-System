532c6c351f0993c8e5bd6c6c22aeab28abd8af27
ColePriser <cpriser@purdue.edu>
2022-05-22 17:08:35 -0400

    Added method to create a text file for each teacher object

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,9 +1,20 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Teacher extends Person {
     private ArrayList<TeacherCourse> courses;
-    public Teacher(int ID, String name, String email, String password) {
-        super(ID, name, email, password);
+    public Teacher(String name, int ID, String email, String password) {
+        super(name, ID, email, password);
         this.courses = null;
+    }
+
+    public void createTextFile() {
+        try {
+            File text = new File(super.getTextFile());
+            text.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
