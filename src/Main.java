7f93103591a1bace0f5c772617e1136345a4dea2
ColePriser <cpriser@purdue.edu>
2022-05-22 17:10:01 -0400

    Added some comments

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,11 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(2361);
+        ServerSocket serverSocket = new ServerSocket(2361); //ServerSocket object
+        /*
+         * Creates an infinite loop for multiple clients to connect
+         * to the server. This is necessary for a multithreaded server
+         */
         while (true) {
             Socket socket = serverSocket.accept();
             Server server = new Server(socket);
