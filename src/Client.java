cf97dd06545b6481dff459eea4c9c7ee0ce121fd
ColePriser <cpriser@purdue.edu>
2022-05-24 16:12:50 -0500

    Implemented starter menus for student and teachers for singing in and creating accounts

--- src/Client.java
+++ src/Client.java
@@ -11,19 +11,23 @@ public class Client implements Runnable {
     private static PrintWriter writer; //PrintWriter object
     private static int ID; //ID of client
 
+    private static JFrame mainMenu = new JFrame("Course Management System Main Menu");
+    private static JFrame teacherMenu = new JFrame("Teacher Menu");
+    private static JFrame studentMenu = new JFrame("Student Menu");
+
     public static void main(String[] args) {
         Thread client = new Thread(new Client());
         client.start();
         try {
-            socket = new Socket("localhost", 2361);
-            writer = new PrintWriter(socket.getOutputStream());
+            Socket temp = new Socket("localhost", 2361);
+            socket = temp;
             bfr = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            writer = new PrintWriter(socket.getOutputStream());
             /*
              * Creating socket and setting up reading and writing capabilities
              * to communicate with the server.
              */
             JPanel mainMenuPanel = new JPanel();
-            JFrame mainMenu = new JFrame("Course Management System Main Menu");
             Container mainMenuContainer = mainMenu.getContentPane();
             mainMenuContainer.setLayout(new BorderLayout());
             mainMenu.setSize(300, 200);
@@ -43,8 +47,10 @@ public class Client implements Runnable {
              * email, password, and ID. This information is sent to the server.
              */
             ActionListener createAccountListener = new ActionListener() {
+                @Override
                 public void actionPerformed(ActionEvent e) {
                     if (e.getSource() == createAccountButton) {
+                        mainMenu.setVisible(false);
                         String[] accountType = {"Student", "Teacher"};
                         String type = (String) JOptionPane.showInputDialog(null, "Select your account type",
                                 "Create Account", JOptionPane.QUESTION_MESSAGE, null, accountType,
@@ -83,13 +89,12 @@ public class Client implements Runnable {
                         boolean IDError = true;
                         boolean IDLengthError = true;
                         boolean IDStartNumError = true;
-                        boolean numError = false;
                         do {
                             IDError = false;
                             IDLengthError = false;
                             IDStartNumError = false;
                             try {
-                                ID = Integer.parseInt(JOptionPane.showInputDialog(null, "What is your student ID?",
+                                ID = Integer.parseInt(JOptionPane.showInputDialog(null, "What is your ID?",
                                         "Create Account",
                                         JOptionPane.QUESTION_MESSAGE));
                             } catch (NumberFormatException ime) {
@@ -135,7 +140,7 @@ public class Client implements Runnable {
                         writer.write(password);
                         writer.println();
                         writer.flush();
-                        writer.write(ID);
+                        writer.write(Integer.toString(ID));
                         writer.println();
                         writer.flush();
                     }
@@ -150,49 +155,55 @@ public class Client implements Runnable {
             ActionListener signInListener = new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
                     if (e.getSource() == signInButton) {
+                        mainMenu.setVisible(false);
+                        String[] accountType = {"Student", "Teacher"};
+                        String type = (String) JOptionPane.showInputDialog(null, "Select your account type",
+                                "Create Account", JOptionPane.QUESTION_MESSAGE, null, accountType,
+                                accountType[0]);
                         int ID = 0;
                         boolean IDError = true;
                         boolean IDLengthError = true;
-                        boolean IDStartNumError = true;
-                        boolean numError = false;
                         do {
                             IDError = false;
                             IDLengthError = false;
-                            IDStartNumError = false;
                             try {
-                                ID = Integer.parseInt(JOptionPane.showInputDialog(null, "What is your student ID?",
-                                        "Create Account",
+                                ID = Integer.parseInt(JOptionPane.showInputDialog(null, "What is your ID?",
+                                        "Sign In",
                                         JOptionPane.QUESTION_MESSAGE));
                             } catch (NumberFormatException ime) {
-                                JOptionPane.showMessageDialog(null, "ID can only contain numbers!", "Create Account",
+                                JOptionPane.showMessageDialog(null, "ID can only contain numbers!", "Sign In",
                                         JOptionPane.ERROR_MESSAGE);
                                 IDError = true;
                             }
                             if (!IDError && String.valueOf(ID).length() != 7) {
-                                JOptionPane.showMessageDialog(null, "ID must have length of 7!", "Create Account",
+                                JOptionPane.showMessageDialog(null, "ID must have length of 7!", "Sign In",
                                         JOptionPane.ERROR_MESSAGE);
                                 IDLengthError = true;
                             }
                         } while (IDError || IDLengthError);
-                        String password;
+                        String password = "";
                         do {
-                            password = JOptionPane.showInputDialog(null, "Enter your desired password.",
-                                    "Create Account", JOptionPane.QUESTION_MESSAGE);
+                            password = JOptionPane.showInputDialog(null, "Enter your password.",
+                                    "Sign In", JOptionPane.QUESTION_MESSAGE);
                             if ((password == null) || (password.isBlank())) {
-                                JOptionPane.showMessageDialog(null, "Password cannot be empty!", "Create Account",
+                                JOptionPane.showMessageDialog(null, "Password cannot be empty!", "Sign In",
                                         JOptionPane.ERROR_MESSAGE);
-
                             }
                         } while ((password == null) || (password.isBlank()));
-                        writer.write("Sign In");
+                        if (type.equals("Teacher")) {
+                            writer.write("Sign In To Teacher Account");
+                        } else {
+                            writer.write("Sign In To Student Account");
+                        }
                         writer.println();
                         writer.flush();
-                        writer.write(ID);
+                        writer.write(Integer.toString(ID));
                         writer.println();
                         writer.flush();
                         writer.write(password);
                         writer.println();
                         writer.flush();
+
                     }
                 }
             };
@@ -201,26 +212,61 @@ public class Client implements Runnable {
             e.printStackTrace();
         }
     }
+
     public void run() {
         while (true) {
             try {
-                String messageFromServer = "temp";
+                String messageFromServer;
                 try {
                     messageFromServer = bfr.readLine();
-                } catch (IOException ex) {
+                } catch (Exception e) {
                     continue;
                 }
-                System.out.println(messageFromServer);
                 if (messageFromServer.equals("Teacher ID Taken")) {
                     JOptionPane.showMessageDialog(null,
                             "Failed to create account! There already exists a teacher with this ID!",
-                            "Create Account",
-                            JOptionPane.ERROR_MESSAGE);
+                            "Create Account", JOptionPane.ERROR_MESSAGE);
+                    mainMenu.setVisible(true);
                 } else if (messageFromServer.equals("Teacher Account Created Success")) {
                     JOptionPane.showMessageDialog(null,
                             "Your account has been made successfully!",
-                            "Create Account",
-                            JOptionPane.INFORMATION_MESSAGE);
+                            "Create Account", JOptionPane.INFORMATION_MESSAGE);
+                    mainMenu.setVisible(true);
+                } else if (messageFromServer.equals("Student ID Taken")) {
+                    JOptionPane.showMessageDialog(null,
+                            "Failed to create account! There already exists a student with this ID!",
+                            "Create Account", JOptionPane.ERROR_MESSAGE);
+                    mainMenu.setVisible(true);
+                } else if (messageFromServer.equals("Student Account Created Success")) {
+                    JOptionPane.showMessageDialog(null,
+                            "Your account has been made successfully!",
+                            "Create Account", JOptionPane.INFORMATION_MESSAGE);
+                    mainMenu.setVisible(true);
+                } else if (messageFromServer.equals("Teacher Sign In Success")) {
+                    JOptionPane.showMessageDialog(null,
+                            "You have signed in successfully!",
+                            "Sign In", JOptionPane.INFORMATION_MESSAGE);
+                    mainMenu.setVisible(false);
+                    teacherMenu.setVisible(true);
+                }
+                else if (messageFromServer.equals("Teacher Sign In Failed")) {
+                    JOptionPane.showMessageDialog(null,
+                            "No teacher account exists with the given ID and password.",
+                            "Create Account", JOptionPane.ERROR_MESSAGE);
+                    mainMenu.setVisible(true);
+                }
+                else if (messageFromServer.equals("Student Sign In Success")) {
+                    JOptionPane.showMessageDialog(null,
+                            "You have signed in successfully!",
+                            "Sign In", JOptionPane.INFORMATION_MESSAGE);
+                    mainMenu.setVisible(false);
+                    studentMenu.setVisible(true);
+                }
+                else if (messageFromServer.equals("Student Sign In Failed")) {
+                    JOptionPane.showMessageDialog(null,
+                            "No student account exists with the given ID and password.",
+                            "Create Account", JOptionPane.ERROR_MESSAGE);
+                    mainMenu.setVisible(true);
                 }
             } catch (Exception e) {
                 e.printStackTrace();
--- src/Server.java
+++ src/Server.java
@@ -1,6 +1,5 @@
 import java.net.*;
 import java.io.*;
-import java.net.*;
 import java.util.*;
 
 public class Server implements Runnable {
@@ -10,11 +9,11 @@ public class Server implements Runnable {
     private static ArrayList<Socket> clients = new ArrayList<>(); //ArrayList of clients
     private static ArrayList<Teacher> teachers = new ArrayList<>(); //ArrayList of teachers
     private static ArrayList<Student> students = new ArrayList<>(); //ArrayList of students
-    private static Object race = new Object(); //Object used in synchronized blocks to prevent race conditions
+    private static final Object race = new Object(); //Object used in synchronized blocks to prevent race conditions
 
     public Server(Socket inputSocket) {
         this.socket = inputSocket;
-        clients.add(this.socket);
+        clients.add(inputSocket);
         try {
             this.bfr = new BufferedReader(new InputStreamReader(inputSocket.getInputStream()));
             this.writer = new PrintWriter(inputSocket.getOutputStream());
@@ -27,40 +26,107 @@ public class Server implements Runnable {
         while (true) {
             try {
                 String messageFromClient = bfr.readLine();
-                if (messageFromClient.equals("Create Teacher Account")) {
-                    String name = bfr.readLine();
-                    String email = bfr.readLine();
-                    String password = bfr.readLine();
-                    int ID = Integer.parseInt(bfr.readLine());
-                    boolean taken = false;
-                    synchronized (race) {
-                        for (int x = 0; x < teachers.size(); x++) {
-                            if (teachers.get(x).getID() == ID) {
-                                taken = true;
-                                break;
+                switch (messageFromClient) {
+                    case "Create Teacher Account": {
+                        String name = bfr.readLine();
+                        String email = bfr.readLine();
+                        String password = bfr.readLine();
+                        int ID = Integer.parseInt(bfr.readLine());
+                        boolean taken = false;
+                        synchronized (race) {
+                            for (Teacher teacher : teachers) {
+                                if (teacher.getID() == ID) {
+                                    taken = true;
+                                    break;
+                                }
                             }
                         }
-                    }
-                    writer = new PrintWriter(this.socket.getOutputStream());
-                    if (taken) {
-                        writer.write("Teacher ID Taken");
+                        writer = new PrintWriter(this.socket.getOutputStream());
+                        if (taken) {
+                            writer.write("Teacher ID Taken");
+                        } else {
+                            Teacher newAccount = new Teacher(name, ID, email, password);
+                            teachers.add(newAccount);
+                            writer.write("Teacher Account Created Success");
+                        }
                         writer.println();
                         writer.flush();
-                        continue;
+                        break;
                     }
-                    else {
-                        Teacher newAccount = new Teacher(name, ID, email, password);
-                        teachers.add(newAccount);
-                        writer.write("Teacher Account Created Success");
+                    case "Create Student Account": {
+                        String name = bfr.readLine();
+                        String email = bfr.readLine();
+                        String password = bfr.readLine();
+                        int ID = Integer.parseInt(bfr.readLine());
+                        boolean taken = false;
+                        synchronized (race) {
+                            for (Student student : students) {
+                                if (student.getID() == ID) {
+                                    taken = true;
+                                    break;
+                                }
+                            }
+                        }
+                        writer = new PrintWriter(this.socket.getOutputStream());
+                        if (taken) {
+                            writer.write("Student ID Taken");
+                        } else {
+                            Student newAccount = new Student(name, ID, email, password);
+                            students.add(newAccount);
+                            writer.write("Student Account Created Success");
+                        }
                         writer.println();
                         writer.flush();
+                        break;
                     }
-                }
-                else if (messageFromClient.equals("Create Student Account")) {
-
-                }
-                else if (messageFromClient.equals("Sign In")) {
-
+                    case "Sign In To Teacher Account": {
+                        int ID = Integer.parseInt(bfr.readLine());
+                        String password = bfr.readLine();
+                        boolean accountFound = false;
+                        synchronized (race) {
+                            for (Teacher teacher : teachers) {
+                                if (teacher.getID() == ID) {
+                                    if (teacher.getPassword().equals(password)) {
+                                        accountFound = true;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                        writer = new PrintWriter(this.socket.getOutputStream());
+                        if (accountFound) {
+                            writer.write("Teacher Sign In Success");
+                        } else {
+                            writer.write("Teacher Sign In Failed");
+                        }
+                        writer.println();
+                        writer.flush();
+                        break;
+                    }
+                    case "Sign In To Student Account": {
+                        int ID = Integer.parseInt(bfr.readLine());
+                        String password = bfr.readLine();
+                        boolean accountFound = false;
+                        synchronized (race) {
+                            for (Student student : students) {
+                                if (student.getID() == ID) {
+                                    if (student.getPassword().equals(password)) {
+                                        accountFound = true;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                        writer = new PrintWriter(this.socket.getOutputStream());
+                        if (accountFound) {
+                            writer.write("Student Sign In Success");
+                        } else {
+                            writer.write("Student Sign In Failed");
+                        }
+                        writer.println();
+                        writer.flush();
+                        break;
+                    }
                 }
             } catch (Exception e) {
                 e.printStackTrace();
