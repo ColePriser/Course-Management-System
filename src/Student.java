15cee46a5a14fd2410cd7f3a54357ca98bc53b07
ColePriser <cpriser@purdue.edu>
2022-06-09 18:47:57 -0500

    Removed unneeded methods and fields

--- src/Student.java
+++ src/Student.java
@@ -1,20 +1,5 @@
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-
 public class Student extends Person {
-    private ArrayList<TeacherCourse> courses;
     public Student(String name, int ID, String email, String password) {
         super(name, ID, email, password);
-        this.courses = null;
-    }
-
-    public void createTextFile() {
-        try {
-            File text = new File(super.getTextFile());
-            text.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
