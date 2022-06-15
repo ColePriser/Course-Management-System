6114c894524f79d005d7bc5c60ff7bfa05707554
ColePriser <cpriser@purdue.edu>
2022-06-15 11:52:11 -0500

    Created a student menu

---
+++ src/StudentMenu.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class StudentMenu extends JFrame {
+    private JButton viewEnrolledCoursesButton;
+    private JButton enrollInNewCourseButton;
+    private JButton viewGradesButton;
+    private JPanel studentMenuPanel;
+    private JButton returnToMainMenuButton;
+    private JButton settingsButton;
+
+    public StudentMenu() {
+        setContentPane(studentMenuPanel);
+        setTitle("Student Menu");
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        setSize(screenSize.width, screenSize.height);
+        ImageIcon logoIcon = new ImageIcon("book.png");
+        Image logo = logoIcon.getImage();
+        setIconImage(logo);
+        setVisible(false);
+
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+                User.writer.write("Close");
+                User.writer.println();
+                User.writer.flush();
+            }
+        });
+
+        returnToMainMenuButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                User.studentFrame.setVisible(false);
+                User.mainMenuFrame.setVisible(true);
+            }
+        });
+    }
+}
