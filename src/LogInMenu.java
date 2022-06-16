ba84bb1c0af06d85d713a88988b64c2e97db0ec5
ColePriser <cpriser@purdue.edu>
2022-06-16 14:58:52 -0500

    Debugged log in menu

--- src/LogInMenu.java
+++ src/LogInMenu.java
@@ -1,14 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
 
 public class LogInMenu extends JFrame {
     private JCheckBox teacherCheckBox;
     private JTextField IDField;
-    private JPasswordField passwordField1;
+    private JPasswordField passwordField;
     private JPanel logInPanel;
     private JButton backButton;
     private JButton submitButton;
@@ -37,32 +34,65 @@ public class LogInMenu extends JFrame {
         submitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                boolean idError = false;
+                int ID = 0;
+                try {
+                    ID = Integer.parseInt(IDField.getText());
+                } catch (NumberFormatException ime) {
+                    idError = true;
+                }
                 if (studentCheckBox.isSelected() && teacherCheckBox.isSelected()) {
-                    User.writer.write("Log In Menu Both Checked");
-                    User.writer.println();
-                    User.writer.flush();
-                } else if (!studentCheckBox.isSelected() && !teacherCheckBox.isSelected()) {
-                    User.writer.write("Log In Menu Neither Checked");
-                    User.writer.println();
-                    User.writer.flush();
-                } else if (studentCheckBox.isSelected()) {
+                    JOptionPane.showMessageDialog(null,
+                            "Choose either Student or Teacher!",
+                            "Log In", JOptionPane.ERROR_MESSAGE);
+                }
+                else if (!studentCheckBox.isSelected() && !teacherCheckBox.isSelected()) {
+                    JOptionPane.showMessageDialog(null,
+                            "Choose either Student or Teacher!",
+                            "Log In", JOptionPane.ERROR_MESSAGE);
+                }
+                else if (idError) {
+                    JOptionPane.showMessageDialog(null, "ID can only contain numbers!", "Log In",
+                            JOptionPane.ERROR_MESSAGE);
+                }
+                else if (String.valueOf(ID).length() != 7) {
+                    JOptionPane.showMessageDialog(null, "ID must have length of 7!", "Log In",
+                            JOptionPane.ERROR_MESSAGE);
+                }
+                else if (teacherCheckBox.isSelected() && String.valueOf(ID).charAt(0) != '2') {
+                    JOptionPane.showMessageDialog(null, "Teacher Accounts must have ID number that starts with '2'!",
+                            "Log In",
+                            JOptionPane.ERROR_MESSAGE);
+                }
+                else if (studentCheckBox.isSelected() && String.valueOf(ID).charAt(0) != '1') {
+                    JOptionPane.showMessageDialog(null, "Student Accounts must have ID number that starts with '1'!",
+                            "Log In",
+                            JOptionPane.ERROR_MESSAGE);
+
+                }
+                else if (String.valueOf(passwordField.getPassword()).isBlank()) {
+                    JOptionPane.showMessageDialog(null, "Password cannot be empty!",
+                            "Log In", JOptionPane.ERROR_MESSAGE);
+                }
+                else if (studentCheckBox.isSelected()) {
                     User.writer.write("Log In Menu Student Account");
                     User.writer.println();
                     User.writer.flush();
                     User.writer.write(IDField.getText());
                     User.writer.println();
                     User.writer.flush();
-                    User.writer.write(passwordField1.getPassword());
+                    User.writer.write(String.valueOf(passwordField.getPassword()));
                     User.writer.println();
                     User.writer.flush();
-                } else if (teacherCheckBox.isSelected()) {
+                }
+                else if (teacherCheckBox.isSelected()) {
                     User.writer.write("Log In Menu Teacher Account");
                     User.writer.println();
                     User.writer.flush();
                     User.writer.write(IDField.getText());
                     User.writer.println();
                     User.writer.flush();
-                    User.writer.write(passwordField1.getPassword());
+                    User.writer.write(String.valueOf(passwordField.getPassword()));
                     User.writer.println();
                     User.writer.flush();
                 }
@@ -76,5 +106,7 @@ public class LogInMenu extends JFrame {
                 User.mainMenuFrame.setVisible(true);
             }
         });
+
+
     }
 }
