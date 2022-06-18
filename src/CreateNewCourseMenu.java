1931f301ca6cfcd4c111611edab38347f8a24148
ColePriser <cpriser@purdue.edu>
2022-06-18 01:55:44 -0500

    Began implementing a Create New Course Menu

---
+++ src/CreateNewCourseMenu.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class CreateNewCourseMenu {
+    private JTextField nameField;
+    private JTextField idField;
+    private JButton createButton;
+    private JButton backButton;
+}
