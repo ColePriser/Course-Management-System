9d2c6db508228e9a70c0cb1aa1d0406722aeaf2c
ColePriser <cpriser@purdue.edu>
2022-06-21 12:01:29 -0500

    Implemented CreateNewCourseMenu to create a new course

--- src/CreateNewCourseMenu.java
+++ src/CreateNewCourseMenu.java
@@ -1,8 +1,88 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-public class CreateNewCourseMenu {
+public class CreateNewCourseMenu extends JFrame {
     private JTextField nameField;
     private JTextField idField;
     private JButton createButton;
     private JButton backButton;
+    private JPanel createNewCoursePanel;
+
+    public CreateNewCourseMenu() {
+        setContentPane(createNewCoursePanel);
+        setTitle("Create Course Menu");
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        setSize(screenSize.width, screenSize.height);
+        ImageIcon logoIcon = new ImageIcon("book.png");
+        Image logo = logoIcon.getImage();
+        setIconImage(logo);
+        setVisible(false);
+
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+                User.writer.write("Close");
+                User.writer.println();
+                User.writer.flush();
+            }
+        });
+
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                User.createCourseFrame.setVisible(false);
+                User.teacherSettingsFrame.setVisible(true);
+            }
+        });
+
+        createButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                boolean idError = false;
+                int ID = 0;
+                try {
+                    ID = Integer.parseInt(idField.getText());
+                } catch (NumberFormatException ime) {
+                    idError = true;
+                }
+                if (nameField.getText() == null || nameField.getText().isBlank()) {
+                    JOptionPane.showMessageDialog(null, "Name cannot be empty!",
+                            "Create Course", JOptionPane.ERROR_MESSAGE);
+                }
+                else if (idError) {
+                    JOptionPane.showMessageDialog(null, "ID can only contain numbers!", "Create Course",
+                            JOptionPane.ERROR_MESSAGE);
+                }
+                else if (String.valueOf(ID).length() != 5) {
+                    JOptionPane.showMessageDialog(null, "Course ID must have length of 5!", "Create Course",
+                            JOptionPane.ERROR_MESSAGE);
+                }
+                else {
+                    User.writer.write("Create Course");
+                    User.writer.println();
+                    User.writer.flush();
+                    User.writer.write(nameField.getText());
+                    User.writer.println();
+                    User.writer.flush();
+                    User.writer.write(idField.getText());
+                    User.writer.println();
+                    User.writer.flush();
+                    User.writer.write(User.userID);
+                    User.writer.println();
+                    User.writer.flush();
+                    resetCreateCourseLabels();
+                }
+            }
+        });
+    }
+
+    public void resetCreateCourseLabels() {
+        nameField.setText("");
+        idField.setText("");
+    }
 }
