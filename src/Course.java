f958704b8231113d4dd6499e102f1f1b9f468092
ColePriser <cpriser@purdue.edu>
2022-06-23 02:55:51 -0500

    Added enrolled students field and added get/set methods

--- src/Course.java
+++ src/Course.java
@@ -1,15 +1,32 @@
+import java.util.ArrayList;
+
 public class Course {
     private String courseName;
     private int courseID;
+    private String coursePassword;
     private Teacher teacher;
+    public static ArrayList<Student> enrolledStudents = new ArrayList<>();
 
-    public Course(String courseName, int courseID, Teacher teacher) {
+    public Course(String courseName, int courseID, String coursePassword, Teacher teacher) {
         this.courseName = courseName;
         this.courseID = courseID;
+        this.coursePassword = coursePassword;
         this.teacher = teacher;
     }
 
     public int getCourseID() {
         return courseID;
+    }
+
+    public ArrayList<Student> getEnrolledStudents() {
+        return enrolledStudents;
+    }
+
+    public String getCoursePassword() {
+        return coursePassword;
+    }
+
+    public void setCoursePassword(String coursePassword) {
+        this.coursePassword = coursePassword;
     }
 }
