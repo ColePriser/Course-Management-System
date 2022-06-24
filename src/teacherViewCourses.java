dd4e28843bff545dd9e2f0a524337d8a6f992ac8
ColePriser <cpriser@purdue.edu>
2022-06-24 00:46:12 -0500

    Began implementing Teacher View Courses Menu

--- src/teacherViewCourses.java
+++ src/teacherViewCourses.java
@@ -22,8 +22,14 @@ public class teacherViewCourses extends JFrame {
     private JLabel courseNameSix;
     private JLabel courseNameSeven;
     private JLabel courseNameEight;
+    private JLabel test;
 
-    private ArrayList<Course> courses = new ArrayList<>();
+    private int userID;
+    private String userName;
+    private String userEmail;
+    private String userPassword;
+    private ArrayList<Course> teacherCourses = new ArrayList<>();
+    private ArrayList<JLabel> nameLabels = new ArrayList<>();
 
     public teacherViewCourses() {
         setContentPane(teacherViewCoursesPanel);
@@ -34,6 +40,15 @@ public class teacherViewCourses extends JFrame {
         Image logo = logoIcon.getImage();
         setIconImage(logo);
         setVisible(false);
+        nameLabels.add(courseNameOne);
+        nameLabels.add(courseNameTwo);
+        nameLabels.add(courseNameThree);
+        nameLabels.add(courseNameFour);
+        nameLabels.add(courseNameFive);
+        nameLabels.add(courseNameSix);
+        nameLabels.add(courseNameSeven);
+        nameLabels.add(courseNameEight);
+        resetCourseList();
 
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         addWindowListener(new WindowAdapter() {
@@ -44,6 +59,35 @@ public class teacherViewCourses extends JFrame {
                 User.writer.flush();
             }
         });
+    }
 
+    public void resetCourseList() {
+        this.teacherCourses = Server.getCourses(userID);
+        for (int x = 0; x < teacherCourses.size(); x++) {
+            nameLabels.get(x).setText(teacherCourses.get(x).getCourseName());
+        }
+        int diff = nameLabels.size() - teacherCourses.size();
+        if (diff > 0) {
+            for (int y = teacherCourses.size(); y < nameLabels.size(); y++) {
+                nameLabels.get(y).setText("");
+                //find a way to hind these panels??
+            }
+        }
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
     }
 }
